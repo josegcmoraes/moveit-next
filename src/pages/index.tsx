351a3acc49ba9a9eec2e64dba3eca6b7af297e98
josegcmoraes <josegcmoraes@gmail.com>
2021-02-23 17:39:40 -0300

    Adicionando arquivos dia 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,30 @@
-import Head from 'next/head';
+
+import styles from '../styles/pages/Home.module.css';
+
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from '../components/Countdown';
+
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"/>
+        <title>Início | move.it</title>
       </Head>
-      
-      <ExperienceBar/>
+      <ExperienceBar />
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
